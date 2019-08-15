8300d12ceac5b4b192f738d5cd220151402f4de1
M4cs <mabridgland@protonmail.com>
2019-08-15 01:06:50 -0400

    New Fixes

--- easymodels/__main__.py
+++ easymodels/__main__.py
@@ -1,18 +1,19 @@
-from easymodels.utils import *
 import requests, time
+from easymodels.utils import *
 import sys
-def start():
-    Menu.main()
-    
-__version__ = "1.3"
 
-if __name__ == "__main__":
+__version__ = "1.3.1"
+
+def start():
     sys.stdout.flush()
     print('Checking For Updates...')
     r = requests.get('https://raw.githubusercontent.com/M4cs/EasyModels/master/version.txt').text.replace('\n', '').replace('\r', '')
     if r != __version__:
-        print('Update Available Please Run "pip install easymodules --upgrade" to update to the latest version!')
+        print('Update Available Please Run "pip install easymodules --upgrade --no-cache-dir" to update to the latest version!')
         time.sleep(1)
     else:
         print('All Up To Date! Starting EasyModels CLI')
+    Menu.main()
+
+if __name__ == "__main__":
     start()
