73a3da3eb0afa6ca7a29c1fbf01e8255f857a5f1
M4cs <mabridgland@protonmail.com>
2019-08-16 01:39:57 -0400

    Update 1.4.2, Fuck you Twine

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
     
 setuptools.setup(
     name="EasyModels",
-    version="1.4.1",
+    version="1.4.2",
     author="Max Bridgland",
     install_requires=[
         'requests==2.21.0',
