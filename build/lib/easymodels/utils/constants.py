6d2db73134a867a7f440c01a2f96e2d174f44af6
M4cs <mabridgland@protonmail.com>
2019-08-18 14:25:10 -0700

    Clean Up 1.4.6

--- build/lib/easymodels/utils/constants.py
+++
@@ -1,8 +0,0 @@
-API_URL = "https://modelzoo.co/api/"
-
-HEADERS = {
-    'accept': 'application/json, text/plain, */*',
-    'referer': 'https://modelzoo.co',
-    'connection': 'keep-alive',
-    'content-type': 'application/json'
-}
